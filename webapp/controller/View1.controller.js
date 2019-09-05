d3058120427a4954cbf0268144ee5b509217d37e
Fábio Luiz Esperati Pagoti <fabiopagoti@gmail.com>
2019-09-05 12:37:58 +0000

    versão de aula

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("ovly.bootstrapping-138.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
